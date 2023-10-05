344252ef04ad07b2f258d4a0d2b383cfc92176e9
Dmitriy <ambrose163@mail.ru>
2023-10-05 10:23:01 +0400

    save

--- HW_4/conftest.py
+++ HW_4/conftest.py
@@ -1,15 +1,19 @@
+import logging
 import pytest
 import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+import requests
 
 with open("./testdata.yaml") as f:
     testdata = yaml.safe_load(f)
     browser = testdata["browser"]
 
-@pytest.fixture(scope="function")
+sess = requests.Session()
+
+@pytest.fixture(scope="session")
 def browser():
     if browser == "firefox":
         service = Service(executable_path=GeckoDriverManager().install())
@@ -21,4 +25,16 @@ def browser():
         driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
+
+@pytest.fixture()
+def login_api():
+    try:
+        response = sess.post(testdata["url_login"],
+                             data={'username': testdata["login"], 'password': testdata["password"]})
+        token = response.json()['token']
+    except:
+        logging.exception("Get token exeption")
+        token = None
+    logging.debug("Token return success")
+    return token
 
