51355c83077e40001bd5cfe22d25f68aeb7f6701
Dmitriy <ambrose163@mail.ru>
2023-10-05 10:29:54 +0400

    save

--- HW_4/test_1.py
+++ HW_4/test_1.py
@@ -2,58 +2,65 @@ import time
 from testpage import OperationsHelper
 import logging
 import yaml
+import send_report
 
 with open("./testdata.yaml") as f:
     testdata = yaml.safe_load(f)
 
-def test_positive(browser):
-    # Проверка алерта с валидными данными contact us
+def test_step1(browser):
+    logging.info("Test1 Starting")
+    testpage = OperationsHelper(browser)
+    testpage.go_to_site()
+    testpage.enter_login("test")
+    testpage.enter_pass("test")
+    testpage.click_login_button()
+    assert testpage.get_error_text() == "401"
+
+def test_step2(browser):
+    # Вход в систему
     logging.info("Test2 Starting")
     testpage = OperationsHelper(browser)
     testpage.go_to_site()
     testpage.enter_login(testdata["login"])
     testpage.enter_pass(testdata["password"])
     testpage.click_login_button()
-    testpage.click_contact_link()
-    time.sleep(2)
+    assert testpage.get_blog_text() == "Blog"
 
-    testpage.enter_name_contact(testdata["contact_name"])
-    testpage.enter_email_contact(testdata["contact_email"])
-    testpage.enter_content_contact(testdata["contact_content"])
-    testpage.click_contactus_button()
-    time.sleep(2)
-    assert testpage.get_allert_text() == "Form successfully submitted"
-
-def test_neg1(browser):
-    # Проверка алерта с пустыми полями contact us
-    logging.info("Test2 Starting")
+def test_step_2_1(browser):
+    # Загрузка изображения в пост
+    logging.info("Test2.1 Starting")
     testpage = OperationsHelper(browser)
     testpage.go_to_site()
     testpage.enter_login(testdata["login"])
     testpage.enter_pass(testdata["password"])
     testpage.click_login_button()
-    testpage.click_contact_link()
+    testpage.click_create_post_button()
+    testpage.enter_title(testdata["post_title"])
+    testpage.enter_description(testdata["post_description"])
+    testpage.enter_content(testdata["post_content"])
     time.sleep(2)
-
-    testpage.click_contactus_button()
+    testpage.download_image()
+    testpage.click_save_button()
+    print(testpage.download_image())
     time.sleep(2)
-    assert testpage.get_allert_text() != "Form successfully submitted"
+    assert 1 == 1
 
-def test_neg2(browser):
-    # Проверка отправки формы с невалидными данными contact us
-    logging.info("Test2 Starting")
+
+
+def test_step3(browser):
+    #Создание поста
+    logging.info("Test3 Starting")
     testpage = OperationsHelper(browser)
     testpage.go_to_site()
     testpage.enter_login(testdata["login"])
     testpage.enter_pass(testdata["password"])
     testpage.click_login_button()
-    testpage.click_contact_link()
+    testpage.click_create_post_button()
+
+    testpage.enter_title(testdata["post_title"])
+    testpage.enter_description(testdata["post_description"])
+    testpage.enter_content(testdata["post_content"])
+    testpage.click_save_button()
     time.sleep(2)
 
-    testpage.enter_name_contact("test")
-    testpage.enter_email_contact("test")
-    testpage.enter_content_contact("test")
-    testpage.click_contactus_button()
-    time.sleep(1)
-    email_label_color = testpage.get_email_label_color()
-    assert email_label_color == 'rgba(183, 28, 28, 1)' # Проверяю, сравнивая цвет лейбла при невалидном вводе емейла
+    assert testpage.get_post_title() == testdata["post_title"]
